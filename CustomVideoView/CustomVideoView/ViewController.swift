e59293e9e88e18cefaf13b4602b4b9f6379dbac5
郝高明 <978235424@qq.com>
2016-11-18 10:56:06 +0800

    视频管理类

---
+++ CustomVideoView/CustomVideoView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomVideoView
+//
+//  Created by haogaoming on 2016/11/18.
+//  Copyright © 2016年 郝高明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
